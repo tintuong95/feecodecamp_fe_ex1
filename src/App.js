d2c01de4d5346d8c0f2b8546c71383b1c7da7ba1
tintuong <tintuong95@gmail.com>
2023-03-11 11:02:42 +0700

    hello

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Jquery from "jquery";
+import { useEffect, useState } from "react";
+import listColor from "./list-color";
+import quote from "./quote";
 
 function App() {
+  const [numOne, setNumOne] = useState();
+  const [numTwo, setNumTwo] = useState();
+  const changeColor = (condition = false) => {
+    const num = randomNumber(1, 15);
+    const numTwo = randomNumber(1, 5);
+    Jquery("body,#new-quote,#social a").css({
+      background: listColor[num],
+      transition: "all 3s",
+    });
+    Jquery("#text,#author").css({
+      color: listColor[num],
+      transition: "all 3s",
+    });
+    if (!condition) {
+      Jquery("#text,#author").css({
+        opacity: 0,
+        transition: "all 3s",
+      });
+    }
+
+    setTimeout(function () {
+      if (!condition) {
+        Jquery("#text").text(quote[numTwo].text);
+        Jquery("#author").text(quote[numTwo].author);
+      }
+      Jquery("#text,#author").css({
+        opacity: 100,
+        transition: "all 3s",
+      });
+    }, 3000);
+  };
+
+  const randomNumber = (start, end) => {
+    return Math.floor(Math.random() * (end - start) + start);
+  };
+
+  useEffect(() => {
+    changeColor(true);
+  }, []);
+
+  useEffect(() => {
+    setNumOne(randomNumber(1, 15));
+    setNumTwo(randomNumber(1, 5));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="quote-box">
+        <div id="quote-box-child">
+          <div id="text">{quote[numTwo]?.text}</div>
+          <div id="author" className="my-5">
+            {quote[numTwo]?.author}
+          </div>
+
+          <div id="social">
+            <div>
+              <a href="twitter.com/intent/tweet" id="tweet-quote">
+                <i className="fa fa-twitter"></i>
+              </a>
+              <a href="#1">
+                <i className="fa fa-tumblr"></i>
+              </a>
+            </div>
+            <button
+              id="new-quote"
+              onClick={() => {
+                changeColor(false);
+              }}
+            >
+              New quote
+            </button>
+          </div>
+        </div>
+        <p id="creator">by hezag </p>
+      </div>
     </div>
   );
 }
---
+++ src/quote.js
@@ -0,0 +1,22 @@
+export default {
+  1: {
+    text: "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+    author: "Mother Teresa",
+  },
+  2: {
+    text: "When you reach the end of your rope, tie a knot in it and hang on.",
+    author: "Franklin D. Roosevelt",
+  },
+  3: {
+    text: "Always remember that you are absolutely unique. Just like everyone else.",
+    author: "Margaret Mead",
+  },
+  4: {
+    text: "Don't judge each day by the harvest you reap but by the seeds that you plant.",
+    author: "Robert Louis Stevenson",
+  },
+  5: {
+    text: "The future belongs to those who believe in the beauty of their dreams",
+    author: "Eleanor Roosevelt",
+  },
+};
